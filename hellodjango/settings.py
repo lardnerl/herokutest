580fd932619a14af28ee5ef07933c0fc02f5dc4d
Hugh Lardner <hughlardner@gmail.com>
2014-06-19 17:31:41 +0100

    my django app

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -54,7 +54,7 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES =  {'default' : dj_database_url.config(default='postgres://localhost')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
